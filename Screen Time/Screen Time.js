ec5110f96d41c20d7c8a94058290d9312766620b
Yabets Maregn <yabetsanon@gmail.com>
2024-05-10 17:52:42 +0900

    Add space complexity annotation to JavaScript implementation for too_much_screen_time

--- Screen Time/Screen Time.js
+++ Screen Time/Screen Time.js
@@ -1,4 +1,5 @@
 // Time Complexity: O(n), where n is the number of days (typically 7).
+// Space Complexity: O(1), only a constant amount of extra space is used.
 // JavaScript implementation
 function tooMuchScreenTime(hours) {
   // Rule 1: any single day >= 10
