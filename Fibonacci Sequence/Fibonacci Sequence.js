ec4c0b69252ff13f874c637b434549c49039f3f5
Yabets Maregn <yabetsanon@gmail.com>
2024-06-13 20:26:26 +0900

    Update Fibonacci Sequence.js

---
+++ Fibonacci Sequence/Fibonacci Sequence.js
@@ -0,0 +1,15 @@
+function fibonacciSequence(start, length) {
+  if (length === 0) return [];
+  if (length === 1) return [start[0]];
+  if (length === 2) return [start[0], start[1]];
+
+  let seq = [start[0], start[1]];
+  while (seq.length < length) {
+    let n = seq.length;
+    seq.push(seq[n - 1] + seq[n - 2]);
+  }
+  return seq;
+}
+
+console.log(fibonacciSequence([0, 1], 10));
+console.log(fibonacciSequence([123456789, 987654321], 5));
