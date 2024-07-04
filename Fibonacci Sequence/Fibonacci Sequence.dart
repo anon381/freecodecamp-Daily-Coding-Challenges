c01a2febe537af69319a549f289e7cf99aa1a1a8
Yabets Maregn <yabetsanon@gmail.com>
2024-07-04 20:26:04 +0900

    Update Fibonacci Sequence.dart

---
+++ Fibonacci Sequence/Fibonacci Sequence.dart
@@ -0,0 +1,17 @@
+List<BigInt> fibonacciSequence(List<BigInt> start, int length) {
+  if (length == 0) return [];
+  if (length == 1) return [start[0]];
+  if (length == 2) return [start[0], start[1]];
+
+  List<BigInt> seq = [start[0], start[1]];
+  while (seq.length < length) {
+    int n = seq.length;
+    seq.add(seq[n - 1] + seq[n - 2]);
+  }
+  return seq;
+}
+
+void main() {
+  print(fibonacciSequence([BigInt.from(0), BigInt.from(1)], 10));
+  print(fibonacciSequence([BigInt.from(123456789), BigInt.from(987654321)], 5));
+}
