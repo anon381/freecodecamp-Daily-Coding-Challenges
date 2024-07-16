027827b1debab026a7587ca3dd53cdebf03e39c6
Yabets Maregn <yabetsanon@gmail.com>
2024-07-16 20:02:04 +0900

    Update Base Check.dart

---
+++ Base Check/Base Check.dart
@@ -0,0 +1,24 @@
+bool isValidNumber(String s, int base) {
+  s = s.toUpperCase();
+  for (int i = 0; i < s.length; i++) {
+    String ch = s[i];
+    int val;
+    if (RegExp(r'[0-9]').hasMatch(ch)) {
+      val = int.parse(ch);
+    } else if (RegExp(r'[A-Z]').hasMatch(ch)) {
+      val = 10 + (ch.codeUnitAt(0) - 'A'.codeUnitAt(0));
+    } else {
+      return false;
+    }
+    if (val >= base) return false;
+  }
+  return true;
+}
+
+void main() {
+  print(isValidNumber("10101", 2));   // true
+  print(isValidNumber("10201", 2));   // false
+  print(isValidNumber("ABC", 16));    // true
+  print(isValidNumber("5G3F8F", 16)); // false
+  print(isValidNumber("5G3F8F", 17)); // true
+}
