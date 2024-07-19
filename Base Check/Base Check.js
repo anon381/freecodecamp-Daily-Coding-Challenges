ad7c1bc5279ef0fbd8debecd1d7b6a95d8c3bde9
Yabets Maregn <yabetsanon@gmail.com>
2024-07-19 20:02:22 +0900

    Update Base Check.js

---
+++ Base Check/Base Check.js
@@ -0,0 +1,22 @@
+function isValidNumber(s, base) {
+  s = s.toUpperCase();
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+    let val;
+    if (/[0-9]/.test(c)) {
+      val = parseInt(c, 10);
+    } else if (/[A-Z]/.test(c)) {
+      val = 10 + (c.charCodeAt(0) - "A".charCodeAt(0));
+    } else {
+      return false;
+    }
+    if (val >= base) return false;
+  }
+  return true;
+}
+
+console.log(isValidNumber("10101", 2));   // true
+console.log(isValidNumber("10201", 2));   // false
+console.log(isValidNumber("ABC", 16));    // true
+console.log(isValidNumber("5G3F8F", 16)); // false
+console.log(isValidNumber("5G3F8F", 17)); // true
