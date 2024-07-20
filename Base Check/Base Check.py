24011075de43436002b281bb970932c024739164
Yabets Maregn <yabetsanon@gmail.com>
2024-07-20 20:02:31 +0900

    Update Base Check.py

---
+++ Base Check/Base Check.py
@@ -0,0 +1,7 @@
+def is_valid_number(s: str, base: int) -> bool:
+    s = s.upper()
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    valid_digits = digits[:base]
+    
+    return all(ch in valid_digits for ch in s)
