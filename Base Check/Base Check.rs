eea515a3684e90152e39a0c8400d052d9a21c105
Yabets Maregn <yabetsanon@gmail.com>
2024-07-27 20:02:40 +0900

    Update Base Check.rs

---
+++ Base Check/Base Check.rs
@@ -0,0 +1,21 @@
+fn is_valid_number(s: &str, base: u32) -> bool {
+    for ch in s.chars() {
+        let val = match ch.to_ascii_uppercase() {
+            '0'..='9' => ch as u32 - '0' as u32,
+            'A'..='Z' => 10 + (ch as u32 - 'A' as u32),
+            _ => return false,
+        };
+        if val >= base {
+            return false;
+        }
+    }
+    true
+}
+
+fn main() {
+    println!("{}", is_valid_number("10101", 2));   // true
+    println!("{}", is_valid_number("10201", 2));   // false
+    println!("{}", is_valid_number("ABC", 16));    // true
+    println!("{}", is_valid_number("5G3F8F", 16)); // false
+    println!("{}", is_valid_number("5G3F8F", 17)); // true
+}
