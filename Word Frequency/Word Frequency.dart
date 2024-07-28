70de89ae450dcde4abfef6633d274c1b03d0c6f6
Yabets Maregn <yabetsanon@gmail.com>
2024-07-28 18:44:44 +0900

    Add implementation for Word Frequency in Dart

---
+++ Word Frequency/Word Frequency.dart
@@ -0,0 +1,33 @@
+import 'dart:collection';
+
+List<String> getWords(String paragraph) {
+	// Lowercase and remove punctuation
+	var text = paragraph.toLowerCase().replaceAll(RegExp(r'[,.!]'), '');
+	var words = text.split(RegExp(r'\s+'));
+
+	// Count frequencies
+	var counts = <String, int>{};
+	for (var w in words) {
+		counts[w] = (counts[w] ?? 0) + 1;
+	}
+
+	// Preserve first occurrence order
+	var uniqueWords = <String>[];
+	for (var w in words) {
+		if (!uniqueWords.contains(w)) uniqueWords.add(w);
+	}
+
+	// Sort: frequency desc, then by first occurrence
+	uniqueWords.sort((a, b) {
+		var cmp = counts[b]!.compareTo(counts[a]!);
+		if (cmp != 0) return cmp;
+		return uniqueWords.indexOf(a).compareTo(uniqueWords.indexOf(b));
+	});
+
+	// Return top 3
+	return uniqueWords.take(3).toList();
+}
+
+void main() {
+	print(getWords('Hello world! Hello, code. Hello code world.'));
+}
