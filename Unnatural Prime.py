54fc7b50aba742fb2441c6d39b3449c77652fe06
Yabets Maregn <yabetsanon@gmail.com>
2024-08-18 12:00:00 +0900

    Add is_unnatural_prime to Unnatural Prime.py

---
+++ Unnatural Prime.py
@@ -0,0 +1,12 @@
+def is_unnatural_prime(n: int) -> bool:
+	# 0 and ±1 are not prime
+	if n in (0, 1, -1):
+		return False
+	num = abs(n)  # handle negative numbers
+	if num < 2:
+		return False
+	# check divisibility up to sqrt(num)
+	for i in range(2, int(num**0.5) + 1):
+		if num % i == 0:
+			return False
+	return True
