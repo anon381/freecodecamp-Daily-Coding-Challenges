1b060ddb7b4ceb9a32baf014197aa54e2da09410
Yabets Maregn <yabetsanon@gmail.com>
2024-08-20 12:00:00 +0900

    Add is_unnatural_prime to Unnatural Prime.c

---
+++ Unnatural Prime.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <math.h>
+
+// Checks if a number is an "unnatural prime" (prime for both positive and negative values)
+bool is_unnatural_prime(int n) {
+	// 0 and ±1 are not prime
+	if (n == 0 || n == 1 || n == -1) {
+		return false;
+	}
+	int num = n < 0 ? -n : n; // handle negative numbers
+	if (num < 2) {
+		return false;
+	}
+	// check divisibility up to sqrt(num)
+	int sqrt_num = (int)sqrt((double)num);
+	for (int i = 2; i <= sqrt_num; i++) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
