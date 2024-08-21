e2d66374af2ad4848c62da95d228814acd281278
Yabets Maregn <yabetsanon@gmail.com>
2024-08-21 12:00:00 +0900

    Add is_unnatural_prime to Unnatural Prime.java

---
+++ Unnatural Prime.java
@@ -0,0 +1,20 @@
+// Checks if a number is an "unnatural prime" (prime for both positive and negative values)
+public class UnnaturalPrime {
+	public static boolean isUnnaturalPrime(int n) {
+		// 0 and ±1 are not prime
+		if (n == 0 || n == 1 || n == -1) {
+			return false;
+		}
+		int num = Math.abs(n); // handle negative numbers
+		if (num < 2) {
+			return false;
+		}
+		// check divisibility up to sqrt(num)
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
