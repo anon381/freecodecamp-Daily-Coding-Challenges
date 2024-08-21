a4ee427a87022121ba92729e625b485417951513
Yabets Maregn <yabetsanon@gmail.com>
2024-08-21 12:00:00 +0900

    Add time complexity annotation to Unnatural Prime.java

--- Unnatural Prime.java
+++ Unnatural Prime.java
@@ -1,4 +1,5 @@
 // Checks if a number is an "unnatural prime" (prime for both positive and negative values)
+// Time Complexity: O(sqrt(n))
 public class UnnaturalPrime {
 	public static boolean isUnnaturalPrime(int n) {
 		// 0 and ±1 are not prime
