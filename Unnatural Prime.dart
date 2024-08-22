2d5ff12b59f54c406871987ab61196240d166a46
Yabets Maregn <yabetsanon@gmail.com>
2024-08-22 12:00:00 +0900

    Add is_unnatural_prime to Unnatural Prime.dart

---
+++ Unnatural Prime.dart
@@ -0,0 +1,18 @@
+// Checks if a number is an "unnatural prime" (prime for both positive and negative values)
+bool isUnnaturalPrime(int n) {
+	// 0 and ±1 are not prime
+	if (n == 0 || n == 1 || n == -1) {
+		return false;
+	}
+	int num = n.abs(); // handle negative numbers
+	if (num < 2) {
+		return false;
+	}
+	// check divisibility up to sqrt(num)
+	for (int i = 2; i <= num.sqrt(); i++) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
