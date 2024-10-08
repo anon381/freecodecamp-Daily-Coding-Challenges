50024593e994fdb730ea9fdd11b21e16af755a00
Yabets Maregn <yabetsanon@gmail.com>
2024-10-08 12:00:00 +0900

    Add decode implementation to Message Decoder.c

---
+++ Message Decoder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// Decodes a message by shifting alphabetic characters backwards by 'shift'.
+void decode(const char* message, int shift, char* output) {
+	int len = strlen(message);
+	for (int i = 0; i < len; i++) {
+		char ch = message[i];
+		if (isalpha(ch)) {
+			char base = isupper(ch) ? 'A' : 'a';
+			char decoded = ((ch - base - shift + 26) % 26) + base;
+			output[i] = decoded;
+		} else {
+			output[i] = ch;
+		}
+	}
+	output[len] = '\0';
+}
