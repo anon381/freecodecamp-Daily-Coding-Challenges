806e424757271ca570da24e95e01c0fb92f1954b
Yabets Maregn <yabetsanon@gmail.com>
2024-10-08 19:36:30 +0900

    Update Vowel Balance.java

---
+++ Vowel Balance/Vowel Balance.java
@@ -0,0 +1,27 @@
+public class VowelBalance {
+    public static boolean isBalanced(String s) {
+        String vowels = "aeiouAEIOU";
+        int n = s.length();
+        int half = n / 2;
+
+        String first = s.substring(0, half);
+        String second = s.substring(n - half);
+
+        int countFirst = 0, countSecond = 0;
+
+        for (char c : first.toCharArray()) {
+            if (vowels.indexOf(c) != -1) countFirst++;
+        }
+        for (char c : second.toCharArray()) {
+            if (vowels.indexOf(c) != -1) countSecond++;
+        }
+
+        return countFirst == countSecond;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBalanced("racecar")); // true
+        System.out.println(isBalanced("Lorem Ipsum")); // true
+        System.out.println(isBalanced("Kitty Ipsum")); // false
+    }
+}
