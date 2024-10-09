4e1e8434ad49e786a2ca00c3af4ecc16997886af
Yabets Maregn <yabetsanon@gmail.com>
2024-10-09 19:41:12 +0900

    Add space complexity comment to Vowel Balance.js

--- Vowel Balance/Vowel Balance.js
+++ Vowel Balance/Vowel Balance.js
@@ -1,4 +1,5 @@
 // Time Complexity: O(n), where n is the length of the input string
+// Space Complexity: O(1), only a few variables are used
 function isBalanced(s) {
   const vowels = "aeiouAEIOU";
   const n = s.length;
