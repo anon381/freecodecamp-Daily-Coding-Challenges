2813dc3df15ea5db6fd14618df082539e8de244c
Yabets Maregn <yabetsanon@gmail.com>
2024-10-09 12:00:00 +0900

    Add decode implementation to Message Decoder.java

---
+++ Message Decoder.java
@@ -0,0 +1,16 @@
+// Decodes a message by shifting alphabetic characters backwards by 'shift'.
+public class MessageDecoder {
+	public static String decode(String message, int shift) {
+		StringBuilder result = new StringBuilder();
+		for (char ch : message.toCharArray()) {
+			if (Character.isLetter(ch)) {
+				char base = Character.isUpperCase(ch) ? 'A' : 'a';
+				char decodedChar = (char)(((ch - base - shift + 26) % 26) + base);
+				result.append(decodedChar);
+			} else {
+				result.append(ch);
+			}
+		}
+		return result.toString();
+	}
+}
