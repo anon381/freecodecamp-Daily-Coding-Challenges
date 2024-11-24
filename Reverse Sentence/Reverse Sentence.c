7c7aa37c3783e96a57f92827150e01cac1a1652d
Yabets Maregn <yabetsanon@gmail.com>
2024-11-24 17:31:42 +0900

    Add C implementation for Reverse Sentence

---
+++ Reverse Sentence/Reverse Sentence.c
@@ -0,0 +1,30 @@
+// C implementation
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void reverse_sentence(const char *s) {
+    char words[100][100];
+    int word_count = 0, i = 0, j = 0;
+    int len = strlen(s);
+    while (i < len) {
+        while (i < len && isspace(s[i])) i++; // skip spaces
+        if (i >= len) break;
+        j = 0;
+        while (i < len && !isspace(s[i])) {
+            words[word_count][j++] = s[i++];
+        }
+        words[word_count][j] = '\0';
+        word_count++;
+    }
+    for (int k = word_count - 1; k >= 0; k--) {
+        printf("%s", words[k]);
+        if (k > 0) printf(" ");
+    }
+    printf("\n");
+}
+
+int main() {
+    reverse_sentence("hello   world!  this is   c"); // Output: 'c is this world! hello'
+    return 0;
+}
