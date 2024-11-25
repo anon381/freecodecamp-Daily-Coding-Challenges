34e18c24237475fc11fb34218d31b15858fed98f
Yabets Maregn <yabetsanon@gmail.com>
2024-11-25 17:33:36 +0900

    Add time complexity annotation to Dart implementation for Reverse Sentence

--- Reverse Sentence/Reverse Sentence.dart
+++ Reverse Sentence/Reverse Sentence.dart
@@ -1,3 +1,4 @@
+// Time Complexity: O(n), where n is the number of characters in the input string.
 // Dart implementation
 String reverseSentence(String s) {
   var words = s.split(RegExp(r'\s+')).where((w) => w.isNotEmpty).toList();
