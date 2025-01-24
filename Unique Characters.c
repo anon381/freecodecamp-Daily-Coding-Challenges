925284a45da7182b6bc5a1cd37ca48bab881a5d2
Yabets Maregn <yabetsanon@gmail.com>
2025-01-24 12:00:00 +0900

    Add all_unique function and usage example to Unique Characters.c

---
+++ Unique Characters.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool all_unique(const char* s) {
+	int seen[256] = {0};
+	for (int i = 0; s[i]; i++) {
+		unsigned char ch = s[i];
+		if (seen[ch]) return false;
+		seen[ch] = 1;
+	}
+	return true;
+}
+
+int main() {
+	const char* tests[][2] = {
+		{"abc", "1"},
+		{"aA", "1"},
+		{"QwErTy123!@", "1"},
+		{"~!@#$%^&*()_+", "1"},
+		{"hello", "0"},
+		{"freeCodeCamp", "0"},
+		{"!@#*$%^&*()aA", "0"},
+	};
+	for (int i = 0; i < 7; i++) {
+		printf("%s %d EXPECTED: %s\n", tests[i][0], all_unique(tests[i][0]), tests[i][1]);
+	}
+	return 0;
+}
