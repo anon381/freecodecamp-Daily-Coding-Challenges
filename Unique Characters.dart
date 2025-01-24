25a3a3cce65d97479eb5ad864478b49174b83b44
Yabets Maregn <yabetsanon@gmail.com>
2025-01-24 12:00:00 +0900

    Add allUnique function and usage example to Unique Characters.dart

---
+++ Unique Characters.dart
@@ -0,0 +1,24 @@
+bool allUnique(String s) {
+	// Return true if all characters in s are unique (case-sensitive)
+	var seen = <String>{};
+	for (var ch in s.split('')) {
+		if (seen.contains(ch)) return false;
+		seen.add(ch);
+	}
+	return true;
+}
+
+void main() {
+	var tests = [
+		["abc", true],
+		["aA", true],
+		["QwErTy123!@", true],
+		["~!@#$%^&*()_+", true],
+		["hello", false],
+		["freeCodeCamp", false],
+		["!@#*$%^&*()aA", false],
+	];
+	for (var test in tests) {
+		print("${test[0]} ${allUnique(test[0])} EXPECTED: ${test[1]}");
+	}
+}
