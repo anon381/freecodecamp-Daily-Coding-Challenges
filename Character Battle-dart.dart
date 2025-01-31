418db8bac8ae8e068a77d8610829253c530e3025
Yabets Maregn <yabetsanon@gmail.com>
2025-01-31 12:00:00 +0900

    Update Character Battle-dart.dart

---
+++ Character Battle-dart.dart
@@ -0,0 +1,34 @@
+int charStrength(String c) {
+  if (c.length != 1) return 0;
+  var code = c.codeUnitAt(0);
+  if (code >= 'a'.codeUnitAt(0) && code <= 'z'.codeUnitAt(0)) {
+    return code - 'a'.codeUnitAt(0) + 1;
+  } else if (code >= 'A'.codeUnitAt(0) && code <= 'Z'.codeUnitAt(0)) {
+    return code - 'A'.codeUnitAt(0) + 27;
+  } else if (code >= '0'.codeUnitAt(0) && code <= '9'.codeUnitAt(0)) {
+    return code - '0'.codeUnitAt(0);
+  }
+  return 0;
+}
+
+String battle(String your, String opp) {
+  if (your.length > opp.length) return "Opponent retreated";
+  if (your.length < opp.length) return "We retreated";
+
+  int ourWins = 0, theirWins = 0;
+  for (int i = 0; i < your.length; i++) {
+    int y = charStrength(your[i]);
+    int o = charStrength(opp[i]);
+    if (y > o) {
+      ourWins++;
+    } else if (o > y) {
+      theirWins++;
+    }
+  }
+
+  if (ourWins > theirWins) return "We won";
+  if (ourWins < theirWins) return "We lost";
+  return "It was a tie";
+}
+
+
