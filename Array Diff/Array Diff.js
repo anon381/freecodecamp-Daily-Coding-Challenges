7cfe5b5adf0946c7898392106bc70dae21fa20b9
Yabets Maregn <yabetsanon@gmail.com>
2025-02-09 16:14:55 +0900

    Update Array Diff.js

---
+++ Array Diff/Array Diff.js
@@ -0,0 +1,11 @@
+
+
+function arrayDiff(arr1, arr2) {
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+    const diff = new Set([...set1].filter(x => !set2.has(x)).concat([...set2].filter(x => !set1.has(x))));
+    return Array.from(diff).sort((a, b) => a - b);
+}
+
+// Input validation example
+console.log(arrayDiff([1, 2, 3], [3, 4, 5]));
