9248bb2767874fcc49cbef8ed8fd290a23b3c396
Yabets Maregn <yabetsanon@gmail.com>
2025-02-09 16:15:25 +0900

    Update Array Diff.rs

---
+++ Array Diff/Array Diff.rs
@@ -0,0 +1,18 @@
+
+
+fn array_diff(arr1: &[i32], arr2: &[i32]) -> Vec<i32> {
+    use std::collections::HashSet;
+    let set1: HashSet<_> = arr1.iter().cloned().collect();
+    let set2: HashSet<_> = arr2.iter().cloned().collect();
+    let diff: HashSet<_> = set1.symmetric_difference(&set2).cloned().collect();
+    let mut result: Vec<i32> = diff.into_iter().collect();
+    result.sort();
+    result
+}
+
+fn main() {
+    let arr1 = [1, 2, 3];
+    let arr2 = [3, 4, 5];
+    let result = array_diff(&arr1, &arr2);
+    println!("{:?}", result);
+}
