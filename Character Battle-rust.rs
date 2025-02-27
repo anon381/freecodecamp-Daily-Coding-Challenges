7f94c84105b7aeabc4a17d92b280f00fcc959964
Yabets Maregn <yabetsanon@gmail.com>
2025-02-27 12:00:00 +0900

    Update Character Battle-rust.rs

---
+++ Character Battle-rust.rs
@@ -0,0 +1,39 @@
+fn char_strength(c: char) -> i32 {
+    match c {
+        'a'..='z' => (c as u8 - b'a' + 1) as i32,
+        'A'..='Z' => (c as u8 - b'A' + 27) as i32,
+        '0'..='9' => c.to_digit(10).unwrap() as i32,
+        _ => 0,
+    }
+}
+
+fn battle(your: &str, opp: &str) -> &'static str {
+    if your.len() > opp.len() {
+        return "Opponent retreated";
+    } else if your.len() < opp.len() {
+        return "We retreated";
+    }
+
+    let mut our_wins = 0;
+    let mut their_wins = 0;
+
+    for (y, o) in your.chars().zip(opp.chars()) {
+        let ys = char_strength(y);
+        let os = char_strength(o);
+        if ys > os {
+            our_wins += 1;
+        } else if os > ys {
+            their_wins += 1;
+        }
+    }
+
+    if our_wins > their_wins {
+        "We won"
+    } else if our_wins < their_wins {
+        "We lost"
+    } else {
+        "It was a tie"
+    }
+}
+
+
