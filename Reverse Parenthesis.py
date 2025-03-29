e9bb7d18381d4eb74b3ff041aec002a8fd7cda11
Yabets Maregn <yabetsanon@gmail.com>
2025-03-29 12:00:00 +0900

    Add line-by-line comments explaining loops and conditionals in Reverse Parenthesis.py

--- Reverse Parenthesis.py
+++ Reverse Parenthesis.py
@@ -4,16 +4,20 @@
 def decode(s):
     stack = []
 
+    # Iterate through each character in the input string
     for char in s:
+        # If the character is a closing parenthesis
         if char == ')':
-            # pop until matching '('
+            # Prepare to collect characters to be reversed
             temp = []
-            while stack and stack[-1] != '(':
-                temp.append(stack.pop())
-            stack.pop()  # remove '('
-            # push reversed substring back to stack
+            # Pop characters from the stack until an opening parenthesis is found
+            while stack and stack[-1] != '(': 
+                temp.append(stack.pop())  # Add popped character to temp
+            stack.pop()  # Remove the opening parenthesis '(' from the stack
+            # Push the reversed substring (inside parentheses) back onto the stack
             stack.extend(temp)
         else:
+            # If not a closing parenthesis, push the character onto the stack
             stack.append(char)
 
     return ''.join(stack)
