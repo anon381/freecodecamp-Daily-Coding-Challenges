0311b14d519d24768620cab04887064a94f64dc2
Yabets Maregn <yabetsanon@gmail.com>
2025-04-25 22:19:47 +0900

    Add Squares With Three.rs solution

---
+++ Squares With Three/Squares With Three.rs
@@ -0,0 +1,15 @@
+
+fn squares_with_three(n: u32) -> u32 {
+    let mut count = 0;
+    for i in 1..=n {
+        if (i * i).to_string().contains('3') {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn main() {
+    let n = 10;
+    println!("Count: {}", squares_with_three(n));
+}
