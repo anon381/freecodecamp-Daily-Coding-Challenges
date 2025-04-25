ab91acf347a629f907fbe681040a4beb2c48c52a
Yabets Maregn <yabetsanon@gmail.com>
2025-04-25 22:19:39 +0900

    Add Squares With Three.c solution

---
+++ Squares With Three/Squares With Three.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int squares_with_three(int n) {
+    int count = 0;
+    char buffer[32];
+    for (int i = 1; i <= n; i++) {
+        sprintf(buffer, "%d", i * i);
+        if (strchr(buffer, '3')) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n = 10;
+    printf("Count: %d\n", squares_with_three(n));
+    return 0;
+}
