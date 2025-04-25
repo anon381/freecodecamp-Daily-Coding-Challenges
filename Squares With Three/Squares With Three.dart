6e158850a94452d6007ea25b31bc58b9ed989af2
Yabets Maregn <yabetsanon@gmail.com>
2025-04-25 22:19:53 +0900

    Add Squares With Three.dart solution

---
+++ Squares With Three/Squares With Three.dart
@@ -0,0 +1,15 @@
+
+int squaresWithThree(int n) {
+  int count = 0;
+  for (int i = 1; i <= n; i++) {
+    if ((i * i).toString().contains('3')) {
+      count++;
+    }
+  }
+  return count;
+}
+
+void main() {
+  int n = 10;
+  print('Count: [32m${squaresWithThree(n)}');
+}
