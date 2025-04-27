247eac231abd52728cc23eb13846bbcf960da8f5
Yabets Maregn <yabetsanon@gmail.com>
2025-04-27 12:00:00 +0900

    Update Roman Numeral- Parser.dart

---
+++ Roman Numeral- Parser.dart
@@ -0,0 +1,28 @@
+int parseRomanNumeral(String s) {
+  final values = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  };
+
+  int total = 0;
+  int i = 0;
+
+  while (i < s.length) {
+    if (i + 1 < s.length && values[s[i]]! < values[s[i + 1]]!) {
+      total += values[s[i + 1]]! - values[s[i]]!;
+      i += 2;
+    } else {
+      total += values[s[i]]!;
+      i++;
+    }
+  }
+
+  return total;
+}
+
+
