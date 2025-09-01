463aae4d2a82a242561fe37b8168f1a58bc9132c
Yabets Maregn <yabetsanon@gmail.com>
2025-09-01 22:59:26 +0900

    Add tribonacci_sequence function
    Implement tribonacci_sequence function to generate the sequence.

--- Tribonacci Sequence.py
+++ Tribonacci Sequence.py
@@ -1,1 +1,11 @@
 
+def tribonacci_sequence(signature, n):
+    if n == 0:
+        return []
+    if n <= 3:
+        return signature[:n]
+
+    seq = signature[:]
+    while len(seq) < n:
+        seq.append(seq[-1] + seq[-2] + seq[-3])
+    return seq
