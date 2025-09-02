200a7cae4c7955e204da60955a79c4527899ff59
Yabets Maregn <yabetsanon@gmail.com>
2025-09-03 06:27:22 +0900

    Add rgb_to_hex function to convert RGB to Hex

--- RGB to Hex.py
+++ RGB to Hex.py
@@ -1,1 +1,6 @@
+def rgb_to_hex(rgb_str):
+    # Remove "rgb(" and ")" then split into integers
+    r, g, b = map(int, rgb_str.strip("rgb()").split(","))
+    # Format each value as 2-digit hex and concatenate
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
 
