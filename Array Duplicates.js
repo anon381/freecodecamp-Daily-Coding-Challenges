cb654ee010e0d097fdda5ceac06e40208c12558f
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 15:06:52 +0900

    Implement findDuplicates function
    Add a function to find duplicate numbers in an array.

--- Array Duplicates.js
+++ Array Duplicates.js
@@ -1,1 +1,12 @@
+function findDuplicates(arr) {
+    const counts = {};
+        arr.forEach(num => {
+        counts[num] = (counts[num] || 0) + 1;
+    });
+    return Object.keys(counts)
+                 .filter(num => counts[num] > 1)
+                 .map(Number)
+                 .sort((a, b) => a - b);
+}
+
 
