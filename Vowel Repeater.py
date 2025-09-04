9f300deee1cd01244a4618f7b56c822599945d04
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:59:47 +0900

    Add function to repeat vowels in a string

--- Vowel Repeater.py
+++ Vowel Repeater.py
@@ -1,1 +1,17 @@
 
+def repeat_vowels(s):
+    vowels = "aeiouAEIOU"
+    result = []
+    count = 0  
+
+    for char in s:
+        if char in vowels:
+            count += 1
+            
+            result.append(char + char.lower() * (count - 1))
+        else:
+            result.append(char)
+    
+    return ''.join(result)
+
+
