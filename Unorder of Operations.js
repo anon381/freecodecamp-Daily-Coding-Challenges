13e882220e5c412854e228c3c996acd18d5ff1fd
Yabets Maregn <yabetsanon@gmail.com>
2025-09-07 02:37:41 +0900

    Update Unorder of Operations.js

--- Unorder of Operations.js
+++ Unorder of Operations.js
@@ -1,1 +1,31 @@
+function evaluate(numbers, operators) {
+    if (numbers.length === 0) return 0;
+
+    let result = numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+        const op = operators[(i - 1) % operators.length];
+        const num = numbers[i];
+
+        switch (op) {
+            case '+':
+                result += num;
+                break;
+            case '-':
+                result -= num;
+                break;
+            case '*':
+                result *= num;
+                break;
+            case '/':
+                result = Math.floor(result / num); // integer division
+                break;
+            case '%':
+                result %= num;
+                break;
+        }
+    }
+
+    return result;
+}
 
