0c3fb86fa7ae3665f7d88b1212bcd3cb3ae071d4
Yabets Maregn <yabetsanon@gmail.com>
2025-09-07 02:08:39 +0900

    Add function to calculate total burned candles

--- Candlelight.py
+++ Candlelight.py
@@ -1,1 +1,9 @@
-
+def burn_candles(candles: int, make_new: int) -> int:
+    total_burned = 0
+    leftovers = 0
+    while candles > 0:
+        total_burned += candles
+        leftovers += candles
+        candles = leftovers // make_new
+        leftovers = leftovers % make_new
+    return total_burned
