5e67420ca580d80f2a67b615fea90579e65b2ff7
Yabets Maregn <yabetsanon@gmail.com>
2025-09-07 05:57:17 +0900

    Add to_camel_case function for string conversion
    Implement a function to convert strings to camel case.

--- camelCase.py
+++ camelCase.py
@@ -1,1 +1,19 @@
 
+import re
+
+def to_camel_case(s):
+    # Split by space, dash, or underscore (one or more)
+    words = re.split(r'[ _-]+', s)
+    # Skip empty strings from splitting
+    words = [word for word in words if word]
+    
+    # First word lowercase, rest capitalized
+    if not words:
+        return ''
+    
+    first_word = words[0].lower()
+    rest = [word.capitalize() for word in words[1:]]
+    
+    return first_word + ''.join(rest)
+
+
