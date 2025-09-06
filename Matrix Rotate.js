a2bf73f498126ee10c6de5ddaa7e3ca26afd39d1
Yabets Maregn <yabetsanon@gmail.com>
2025-09-06 15:45:14 +0900

    Implement matrix rotation function

--- Matrix Rotate.js
+++ Matrix Rotate.js
@@ -1,1 +1,12 @@
+function rotate(matrix) {
+  const n = matrix.length;
+  const res = Array.from({ length: n }, () => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      res[j][n - 1 - i] = matrix[i][j];
+    }
+  }
+  return res;
+}
 
