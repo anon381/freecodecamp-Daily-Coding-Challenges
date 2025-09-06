dec61c742db46c9381bb3a395ce1440f2a84b4f3
Yabets Maregn <yabetsanon@gmail.com>
2025-09-07 05:48:30 +0900

    Add decode function for reverse parenthesis

--- Reverse Parenthesis.py
+++ Reverse Parenthesis.py
@@ -1,1 +1,17 @@
-Reverse Parenthesis
+def decode(s):
+    stack = []
+
+    for char in s:
+        if char == ')':
+            # pop until matching '('
+            temp = []
+            while stack and stack[-1] != '(':
+                temp.append(stack.pop())
+            stack.pop()  # remove '('
+            # push reversed substring back to stack
+            stack.extend(temp)
+        else:
+            stack.append(char)
+
+    return ''.join(stack)
+
