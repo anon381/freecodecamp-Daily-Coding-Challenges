bd4a18f3645be3b61120029662400614a8747344
Yabets Maregn <yabetsanon@gmail.com>
2025-09-09 00:45:28 +0900

    Add build_acronym function to Acronym Builder.java

---
+++ Acronym Builder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class AcronymBuilder {
+	public static String buildAcronym(String phrase) {
+		Set<String> ignoreWords = new HashSet<>(Arrays.asList("a", "an", "and", "for", "by", "of"));
+		String[] words = phrase.split(" ");
+		StringBuilder acronym = new StringBuilder();
+		for (int i = 0; i < words.length; i++) {
+			String lw = words[i].toLowerCase();
+			if (i > 0 && ignoreWords.contains(lw)) {
+				continue;
+			}
+			acronym.append(Character.toUpperCase(words[i].charAt(0)));
+		}
+		return acronym.toString();
+	}
+}
