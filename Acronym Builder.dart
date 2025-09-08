dbeddd617572d8f383b5997b4f70e528a4231f7b
Yabets Maregn <yabetsanon@gmail.com>
2025-09-09 00:45:27 +0900

    Add build_acronym function to Acronym Builder.dart

---
+++ Acronym Builder.dart
@@ -0,0 +1,13 @@
+String buildAcronym(String phrase) {
+	final ignoreWords = {'a', 'an', 'and', 'for', 'by', 'of'};
+	final words = phrase.split(' ');
+	final acronym = <String>[];
+	for (var i = 0; i < words.length; i++) {
+		final lw = words[i].toLowerCase();
+		if (i > 0 && ignoreWords.contains(lw)) {
+			continue;
+		}
+		acronym.add(words[i][0].toUpperCase());
+	}
+	return acronym.join();
+}
