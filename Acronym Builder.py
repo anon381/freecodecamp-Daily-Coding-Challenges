d3e59c5835e36fe8567e2deee80ff1bb4824d516
Yabets Maregn <yabetsanon@gmail.com>
2025-09-09 00:45:23 +0900

    Add build_acronym function to Acronym Builder.py

---
+++ Acronym Builder.py
@@ -0,0 +1,11 @@
+def build_acronym(phrase: str) -> str:
+	# Words to ignore unless they are the very first word
+	ignore_words = {"a", "an", "and", "for", "by", "of"}
+	words = phrase.split()
+	acronym = []
+	for i, word in enumerate(words):
+		lw = word.lower()
+		if i > 0 and lw in ignore_words:
+			continue
+		acronym.append(word[0].upper())
+	return "".join(acronym)
