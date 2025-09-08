dab3cbf62b8a1020c5963dcfa248cb06ca9ddb63
Yabets Maregn <yabetsanon@gmail.com>
2025-09-09 00:45:29 +0900

    Add build_acronym function to Acronym Builder.rs

---
+++ Acronym Builder.rs
@@ -0,0 +1,13 @@
+fn build_acronym(phrase: &str) -> String {
+	let ignore_words = ["a", "an", "and", "for", "by", "of"];
+	let words: Vec<&str> = phrase.split_whitespace().collect();
+	let mut acronym = String::new();
+	for (i, word) in words.iter().enumerate() {
+		let lw = word.to_lowercase();
+		if i > 0 && ignore_words.contains(&lw.as_str()) {
+			continue;
+		}
+		acronym.push(word.chars().next().unwrap().to_ascii_uppercase());
+	}
+	acronym
+}
